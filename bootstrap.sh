4a9468238c4d0c9972b2df93f2183cc1d750d6b0
vonpupp <vonpupp@gmail.com>
2014-10-25 23:59:34 -0200

    Fixed dirname and REPO_SRC

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+DIRNAME=`dirname $0`
 
 git clone https://github.com/vonpupp/bootstrap.castle setup
-setup/do-bootstrap.sh
-./10-install.sh
-./20-config.sh
+$DIRNAME/setup/do-bootstrap.sh
+$DIRNAME/10-install.sh
+$DIRNAME/20-config.sh
