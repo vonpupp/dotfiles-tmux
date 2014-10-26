f4a789d06b4141c09a2dd00d48397802e9ee45e3
vonpupp <vonpupp@gmail.com>
2014-10-26 00:04:18 -0200

    Fixed dirname on setup clone

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 DIRNAME=`dirname $0`
 
-git clone https://github.com/vonpupp/bootstrap.castle setup
+git clone https://github.com/vonpupp/bootstrap.castle $DIRNAME/setup
 $DIRNAME/setup/do-bootstrap.sh
 $DIRNAME/10-install.sh
 $DIRNAME/20-config.sh
