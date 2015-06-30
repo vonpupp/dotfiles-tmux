4dd7fac0be0fc68372aa9f2e4dd08fe42e67165e
vonpupp <vonpupp@gmail.com>
2015-06-29 22:44:29 -0300

    Simplifying dotfiles

--- 10-install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# PIP (USER)
-#pip install --user git+git://github.com/Lokaltog/powerline
-pip install --user git+git://github.com/tony/tmuxp
--- bootstrap.sh
+++ bootstrap.sh
@@ -5,5 +5,16 @@ source $DIRNAME/.vars
 source ~/.homesick/repos/homeshick/homeshick.sh
 homeshick link $REPO_NAME
 
-$DIRNAME/10-install.sh
-$DIRNAME/20-config.sh
+# PIP (USER)
+pip install --user git+git://github.com/tony/tmuxp
+
+# TPM
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# start a server but don't attach to it
+tmux start-server
+# create a new session but don't attach to it either
+tmux new-session -d
+# install the plugins
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
+# killing the server is not required
+tmux kill-server
